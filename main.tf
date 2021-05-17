96e4f252b31a08848743934776d3ace65c445c79
Anton Popov <popoffanton96@gmail.com>
2021-05-17 16:46:26 +0300

    Deleted unneccessary child module

--- main.tf
+++ main.tf
@@ -2,8 +2,3 @@ provider "aws" {
   region = var.region
   #TODO parametrize provider if possible (GCP, Azure, AWS)
 }
-
-module "jmeter_cluster" {
-  source = "github.com/tony-lems/jmeter-cloudformation"
-
-}
