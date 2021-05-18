2a966b03e8911fdf7997da4a575970b4288240fb
Anton Popov <popoffanton96@gmail.com>
2021-05-18 16:04:22 +0300

    Added additional outputs | Added provisioner instead of user_data usage | Changed instance naming

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,17 @@ output "jmeter_master_id" {
   value = aws_instance.jmeter_master.*.id
 }
 
+output "jmeter_master_addresses" {
+    value = ["${aws_instance.jmeter_master.*.ipv4_address}"]
+}
+
+
 output "jmeter_slave_id" {
   value = aws_instance.jmeter_slave.*.id
+}
+
+output "jmeter_slave_addresses" {
+    value = ["${aws_instance.jmeter_slave.*.ipv4_address}"]
 }
 
 output "jmeter_sg_id" {
