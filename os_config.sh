0a42979233b9afa1da8cb26e6542507a1a56d92f
Anton <popoffanton96@gmail.com>
2021-05-24 15:37:34 +0300

    Added gitigonre | Added JMeter plugin downloading

--- os_config.sh
+++ os_config.sh
@@ -7,14 +7,18 @@ apt install -y openjdk-8-jre
 apt install -y openjdk-8-jdk
 
 #Download JMeter
+echo "Downloading JMeter ${JMETER_VERSION}..."
 curl -s https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz \
         > apache-jmeter-${JMETER_VERSION}.tgz \
     && tar xzf apache-jmeter-${JMETER_VERSION}.tgz \
     && rm apache-jmeter-${JMETER_VERSION}.tgz \
     && sudo mv apache-jmeter-${JMETER_VERSION} ${JMETER_HOME}
+echo "JMeter was successfully downloaded to the ${JMETER_HOME}"
 
 # TODO add JMeter plugins downloading
-
+cd ${JMETER_HOME} \
+    && curl -sL 'http://search.maven.org/remotecontent?filepath=kg/apc/jmeter-plugins-manager/1.3/jmeter-plugins-manager-1.3.jar' \
+        > lib/ext/jmeter-plugins-manager-1.3.jar
 
 #Extend ip ranges
 echo 1025 60999 > /proc/sys/net/ipv4/ip_local_port_range
