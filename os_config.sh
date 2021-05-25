b37f6163deaf75eb7032f60d36427e5d0f221053
Anton <popoffanton96@gmail.com>
2021-05-25 18:02:43 +0300

    Added TODO task and additional logging

--- os_config.sh
+++ os_config.sh
@@ -15,12 +15,15 @@ curl -s https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-${JMETER_V
     && sudo mv apache-jmeter-${JMETER_VERSION} ${JMETER_HOME}
 echo "JMeter was successfully downloaded to the ${JMETER_HOME}"
 
-# TODO add JMeter plugins downloading
+echo "Downloading JMeter plugins..."
 cd ${JMETER_HOME} \
     && curl -sL 'http://search.maven.org/remotecontent?filepath=kg/apc/jmeter-plugins-manager/1.3/jmeter-plugins-manager-1.3.jar' \
         > lib/ext/jmeter-plugins-manager-1.3.jar
 
 #Extend ip ranges
+echo "Configuring OS before performance test executing..."
 echo 1025 60999 > /proc/sys/net/ipv4/ip_local_port_range
 echo 65536 > /proc/sys/net/ipv4/tcp_max_tw_buckets
 echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
+
+# TODO add JMeter test execution
